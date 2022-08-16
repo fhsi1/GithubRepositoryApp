96989cb8f23053fa71b4713f006237ebbf74bb2d
Yujean Cho <eugenie8@kakao.com>
2022-08-16 21:51:29 +0900

    [UPDATE] Add fetchData Implementation

--- GithubRepository/RepositoryListViewController.swift
+++ GithubRepository/RepositoryListViewController.swift
@@ -5,11 +5,14 @@
 //  Created by Yujean Cho on 2022/08/16.
 //
 
-import SnapKit
 import UIKit
+import RxSwift
+import RxCocoa
 
 class RepositoryListViewController: UITableViewController {
     private let organization = "Apple"
+    private let reposigories = BehaviorSubject<[Repository]>(value: [])
+    private let disposeBag = DisposeBag()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,7 +31,59 @@ class RepositoryListViewController: UITableViewController {
     }
     
     @objc func refresh() {
-        
+        DispatchQueue.global(qos: .background).async { [weak self] in
+            guard let self = self else { return }
+            self.fetchRepositories(of: self.organization)
+        }
+    }
+    
+    func fetchRepositories(of organization: String) {
+        Observable.from([organization])
+            .map { origanization -> URL in
+                return URL(string: "https://api.github.com/orgs/\(origanization)/repos")!
+            }
+            .map { url -> URLRequest in
+                var request = URLRequest(url: url)
+                request.httpMethod = "GET"
+                return request
+            }
+            .flatMap { request -> Observable<(response: HTTPURLResponse, data: Data)> in
+                return URLSession.shared.rx.response(request: request)
+            }
+            .filter { responds, _ in
+                return 200..<300 ~= responds.statusCode
+            }
+            .map { _, data -> [[String: Any]] in
+                guard let json = try? JSONSerialization.jsonObject(with: data, options: []),
+                      let result = json as? [[String: Any]] else {
+                    return []
+                }
+                return result
+            }
+            .filter { result in
+                return result.count > 0
+            }
+            .map { objects in
+                return objects.compactMap { dic -> Repository? in
+                    guard let id = dic["id"] as? Int,
+                          let name = dic["name"] as? String,
+                          let description = dic["description"] as? String,
+                          let stargazersCount = dic["stargazers_count"] as? Int,
+                          let language = dic["language"] as? String else {
+                        return nil
+                    }
+                    return Repository(id: id, name: name, description: description, stargazersCount: stargazersCount, language: language)
+                }
+            }
+            .subscribe(onNext: { [weak self] newRepositories in
+                self?.reposigories.onNext(newRepositories)
+                
+                DispatchQueue.main.async {
+                    self?.tableView.reloadData()
+                    self?.refreshControl?.endRefreshing()
+                }
+            })
+            .disposed(by: disposeBag)
     }
 }
 
